47c149e5fdee59c59ae1e558079f7771e235d175
sga-mudu <411221402@gms.ndhu.edu.tw>
2025-08-19 23:46:37 +0800

    new upd

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss()
   ],
-  base:"/product-grid",
+  base: process.env.VITE_BASE_PATH ||"/product-grid",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
